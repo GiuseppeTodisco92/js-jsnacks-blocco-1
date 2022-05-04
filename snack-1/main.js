6833d919a077cc2c9d540805e447b23db4ec3589
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-04 10:31:55 +0100

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,10 @@
+const num1 = Number(prompt("inserisci il primo numero "));
+const num2 = Number(prompt("inserisci il primo numero "));
+
+if(num1 > num2){
+    console.log("il num 1 è piu grande di num 2")
+} if (num1 < num2){
+    console.log("il num 2 è piu grande di num 1")
+} else {
+    console.log("i numeri sono uguali")
+}
